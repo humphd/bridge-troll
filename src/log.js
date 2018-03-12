56b11d9d9e29f793e3ffba7c4eb269b1d1a40f03
David Humphrey <david.humphrey@senecacollege.ca>
2018-03-12 09:49:51 -0400

    Prefer const to var

--- src/log.js
+++ src/log.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Support log levels, see https://www.npmjs.com/package/loglevel#documentation
-var log = require('loglevel');
+const log = require('loglevel');
 
 // Users can override with log.setLevel('warn') or the 'debug', 'error', etc.
 // This is just the default if nothing is passed to us.
